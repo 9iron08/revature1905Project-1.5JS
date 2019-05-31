5d64808f8676e1d3159ca87f1bf26bad1e9ffa7f
9iron08 <edobson95@gmail.com>
2019-05-31 14:24:30 -0400

    finished style

--- dom.js
+++ dom.js
@@ -1,25 +1,28 @@
 "use strict";
-// start DOM for type example button
+// start DOM for type example button\
 let btnTypeTog = document.getElementById('btnType'); //get btn type element and assign it
 let clickedType = false; //create clicked boolean for typed
 //add click event listener
-btnTypeTog.addEventListener("click", () => 
+if (btnTypeTog)
 {
-    //get element that will be changed
-    let exampleText = document.getElementById('figureType');
-    clickedType = !clickedType; //toggle clicked boolean
-    //test whether clicked is true or false
-    if (clickedType)
+    btnTypeTog.addEventListener("click", () => 
     {
-        //if true show the example for type
-        exampleText.style.display='block';
-    }
-    else
-    {
-        // else make it dissapear
-        exampleText.style.display='none';
-    }
-});
+        //get element that will be changed
+        let exampleText = document.getElementById('figureType');
+        clickedType = !clickedType; //toggle clicked boolean
+        //test whether clicked is true or false
+        if (clickedType)
+        {
+            //if true show the example for type;
+            exampleText.style.display='block';
+        }
+        else
+        {
+            // else make it dissapear
+            exampleText.style.display='none';
+        }
+    });
+}
 //end DOM for type example button
 //start DOM for var example button
 let btnVarTog = document.getElementById('btnVar');
